408403a41c894e0c45cfab6dfe17ff36c5bbf6fd
prasad p <prasad@enefce.com>
2014-06-08 22:31:25 +0200

    Abstract class to define constants

---
+++ AlbumSample/src/main/java/com/sample/album/common/Constants.java
@@ -0,0 +1,9 @@
+package com.sample.album.common;
+
+public abstract class Constants {
+
+   public static final int ALBUM_LIST = 0;
+    public static final int SONGS_LIST = 1;
+    public static final int ARTIST_LIST = 2;
+
+}
